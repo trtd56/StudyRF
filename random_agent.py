6c65e242820d9e50cc645519c625498ce985c7e6
trtd56 <hoge@fuga.com>
2017-11-19 21:30:56 +0900

    fix: randomの環境更新をdqnにそろえた

--- random_agent.py
+++ random_agent.py
@@ -3,14 +3,15 @@
 import gym
 env = gym.make('CartPole-v0')
 
-for i_episode in range(20):  # 合計20エピソード環境を実行する
-    observation = env.reset()
-    R = 0  # エピソード毎の報酬の合計値
-    for t in range(100):  # 1エピソード最大100ステップ環境を更新する
+for i in range(10):
+    obs = env.reset()
+    done = False
+    R = 0
+    t = 0
+    while not done and t < 200:
         env.render()
         action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        R += reward  # エピソードの報酬を足していく
-        if done:  # CartPoleの棒の傾きが一定以上になるとエピソード終了
-            print("Episode {} finished. sum of reward is {}".format(i_episode, R))
-            break
+        obs, r, done, _ = env.step(action)
+        R += r
+        t += 1
+    print('episode:', i, 'R:', R)
